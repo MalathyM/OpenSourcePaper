7f13ca99f4edb3ac1437bac34c109932d47caa8f
Mohit Dedhe <43933588+mdedhe@users.noreply.github.com>
2018-10-20 17:42:06 -0400

    Delete readJson_updated.py
    updated the code in the check_updated.py

--- readJson_updated.py
+++
@@ -1,7 +0,0 @@
-import json
-fp=open('/Users/mohitdedhe/Desktop/MohitsDocs/Certification and Projects/VProject/Files/2015-01-07-11.json')
-datam=fp.readlines()
-fp.close()
-
-for line in datam:
-    data=json.loads(line)
