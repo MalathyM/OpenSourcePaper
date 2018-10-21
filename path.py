aa9efbfb0b98e50b3ea9b2b0e37b09f61881ad84
arundhathi-patil <37865001+arundhathi-patil@users.noreply.github.com>
2018-10-20 20:33:45 -0400

    Update path.py
    Added my files local path.

--- path.py
+++ path.py
@@ -1,6 +1,6 @@
 
 Json_Mohit=/Users/mohitdedhe/Desktop/MohitsDocs/Certification and Projects/VProject/Files
 Json_Vivek=
-Json_Arundathi=
+Json_Arundathi=C:\Users\arupatil\Documents\Data Analytics - Github\Data
 Json_Malathy="D:/MS BAIS/GitHub/Data"
 Json_Nameetha=
