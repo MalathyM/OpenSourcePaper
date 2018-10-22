e76d86666662252af55567c2cbeee3be41336a43
Mohit Dedhe <43933588+mdedhe@users.noreply.github.com>
2018-10-22 13:17:24 -0400

    Update path.py

--- path.py
+++ path.py
@@ -2,5 +2,5 @@
 Json_Mohit=/Users/mohitdedhe/Desktop/MohitsDocs/Certification and Projects/VProject/Files
 Json_Vivek=
 Json_Arundathi=C:/Users/arupatil/Documents/Data Analytics - Github/Data
-Json_Malathy="D:/MS BAIS/GitHub/Data"
+Json_Malathy=D:/MS BAIS/GitHub/Data
 Json_Nameetha=
