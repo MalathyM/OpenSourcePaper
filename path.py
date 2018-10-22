3c46d2ddfe866c0b24d97da3ce29b53f2f2423f3
Mohit Dedhe <43933588+mdedhe@users.noreply.github.com>
2018-10-22 13:21:48 -0400

    Update path.py
    updated the path for most of the person

--- path.py
+++ path.py
@@ -1,6 +1,6 @@
 
-Json_Mohit=/Users/mohitdedhe/Desktop/MohitsDocs/Certification and Projects/VProject/Files
-Json_Vivek=
-Json_Arundathi=C:/Users/arupatil/Documents/Data Analytics - Github/Data
-Json_Malathy=D:/MS BAIS/GitHub/Data
-Json_Nameetha=
+Json_Mohit='/Users/mohitdedhe/Desktop/MohitsDocs/Certification and Projects/VProject/Files'
+Json_Vivek='/home/vivek/github-data'
+Json_Arundathi='C:/Users/arupatil/Documents/Data Analytics - Github/Data'
+Json_Malathy='D:/MS BAIS/GitHub/Data'
+Json_Nameetha=''
