e843d727f957c4c8a7a77a688db137973d996c1f
Mohit Dedhe <43933588+mdedhe@users.noreply.github.com>
2018-11-17 19:13:11 -0500

    Update createCsv_gitHubEvent.py

--- folder/createCsv_gitHubEvent.py
+++ folder/createCsv_gitHubEvent.py
@@ -1,3 +1,10 @@
+import json
+import os
+import glob
+import csv
+import os
+import xlwt
+
 projectAI=['keras-team/keras','accord-net/framework','scikit-learn/scikit-learn','Microsoft/CNTK','Reference-LAPACK/lapack-release'
         ,'BVLC/caffe','Theano/Theano','torch/torch7']
 eventType=['CheckRunEvent',
@@ -44,13 +51,24 @@ eventType=['CheckRunEvent',
 'TeamAddEvent',
 'WatchEvent']
 
-import json
-import os
-import glob
-import csv
-import os
-import xlwt
 jsonNlist=[]
+
+if(len(sys.argv)<2):
+    print("Error: .py <parameter> 1. Mohit 2. Vivek's server 3. Arundathi's server 4. Malathy's server 5. Nameetha's server")
+
+if(sys.argv[1]=='1'):
+    json_path=path.Json_Mohit
+elif (sys.argv[1]=='2'):
+    json_path=path.Json_Vivek
+elif (sys.argv[2]=='3'):
+    json_path=path.Json_Arundathi
+elif (sys.argv[3]=='4'):
+    json_path=path.Json_Malathy
+elif (sys.argv[4]=='5'):
+    json_path=path.Json_Nameetha
+else:
+    print('Provide correct parameter')
+
 json_path="/Users/mohitdedhe/Desktop/MohitsDocs/Certification and Projects/VProject/Files"
 json_files=[json_file for json_file in os.listdir(json_path)if json_file.endswith('.json')]
 Workbook=xlwt.Workbook()
