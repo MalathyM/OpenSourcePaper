832d1e1f4c9683868de5332ef7980c8f3ffa747c
Nameetharamachandra <nameethapatel@gmail.com>
2018-11-24 14:45:40 -0500

    CSV Creation
    CSV file is used to write the output

--- folder/createCsv_gitHubEvent.py
+++ folder/createCsv_gitHubEvent.py
@@ -73,38 +73,33 @@ else:
 
 #json_path="/Users/mohitdedhe/Desktop/MohitsDocs/Certification and Projects/VProject/Files"
 json_files=[json_file for json_file in os.listdir(json_path)if json_file.endswith('.json')]
-Workbook=xlwt.Workbook()
-i=0
-c=2
-col=0
-row=1
-worksheet=Workbook.add_sheet('sheet 1')
-worksheet.write(0,0,'Date')
-worksheet.write(0,1,'Hour')
-worksheet.write(0,2,'RepName')
-worksheet.write(0,3,'Event Name')
-Workbook.save('Data2.xls')
-processed_files=0
+# flag counter used to keep track of number of times the header is printed in csv file
+flag=0
+#looping over the json files
 for every_file in json_files:
-    processed_files+=1
-    print(str(processed_files*100/len(json_files)))
-    print(json_path+'/'+every_file)
-    data=open(json_path+'/'+every_file)
+    global flag
+    data=open(json_path+'/'+every_file,encoding='utf-8')
     jsonrecords=data.readlines()
     data.close()
+    #looping over the lines in jsonrecords to find the repository name, date, hour and event
     for line in jsonrecords:
         jsonstrings=json.loads(line)
+        #looping over evry repository present in the json record
         for airproject in projectAI:
             if(jsonstrings['repo']['name']==airproject):
+                #looping over every event in json record
                 for event in eventType:
-                    eve=jsonstrings['type']
                     if(jsonstrings['type']==event):
-                        if(i>0):
-                            row=row+1
-                            col=0
-                        worksheet.write(row,col,every_file[0:10])
-                        worksheet.write(row,col+1,every_file[11:13])
-                        worksheet.write(row,col+2,jsonstrings['repo']['name'])
-                        worksheet.write(row,col+3,eve)
-                        i=1
-Workbook.save('Data2.xls')
+                        Datevalue=every_file[0:10]
+                        Hourvalue=every_file[11:13]
+                        RepNamevalue=jsonstrings['repo']['name']
+                        EventNamevalue=jsonstrings['type']
+                        #creates a new csv file and appends repository name, date, hour and event into csv
+                        with open('finalresult.csv','a',newline='') as csvfile:
+                            fieldnames = ['Date', 'Hour', 'RepName','EventName']
+                            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                            if flag==0:
+                                writer.writeheader()
+                            writer.writerow({'Date':Datevalue, 'Hour':Hourvalue, 'RepName':RepNamevalue,'EventName':EventNamevalue})
+                            flag+=1
+                    
