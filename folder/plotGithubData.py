a7e85b7e89e5b012c1a1e1b52c13464fbe26f0bf
Mohit Dedhe <43933588+mdedhe@users.noreply.github.com>
2018-11-29 21:35:01 -0500

    Update plotGithubData.py

--- folder/plotGithubData.py
+++ folder/plotGithubData.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 df=pd.read_csv('/Users/mohitdedhe/Downloads/finalresult.csv')
 df1=pd.pivot_table(df, index=['RepName','EventName'], values='Hour',aggfunc=len)
-df1
+
+#Change the string value to int
+
+dfFinal['Hour'] = pd.to_numeric(dfFinal['Hour'])
+#Plotting the Cube plot 
+plots1 = dfFinal.pivot('EventName', 'RepName', 'Hour')
+f, ax = plt.subplots(figsize=(10, 9))
+sns.heatmap(plots1, annot=True,  linewidths=.5, ax=ax)
+plt.show()
