bf24acad4a6b812e248790b3742172022414f72e
raghavramky <44325348+raghavramky@users.noreply.github.com>
2018-12-28 14:03:58 -0600

    Add files via upload

--- transaction_module.py
+++ transaction_module.py
@@ -1,23 +1,44 @@
-# -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.remote.webelement import WebElement
+import random
 import time
+#driver = webdriver.Firefox()
 
-chromedriver = "C:/Web Drivers"
 driver = webdriver.Chrome(executable_path = "chromedriver")
 
-driver.get('https://opencollective.com/wwcode/transactions')
+driver.get("https://opencollective.com/discover")
 
-downloadcsv_button = driver.find_element_by_xpath('//a[@class = "download-csv"]')
-downloadcsv_button.click()
-
-#amt = driver.find_elements_by_xpath("//span[contains(@class, 'annualBudget')]")
-
-start_date = driver.find_element_by_xpath("//input[contains(@class, 'form-control')]")
-
+all_links = []
 time.sleep(5)
-start_date.click()
-start_date.clear()
-start_date.send_keys("01/01/2000")
-download_button = driver.find_element_by_xpath('//*[@id="csv-date-range"]/div[2]/button')
-download_button.click()
+
+while (True):
+	#to be commented
+	#if (len(all_links) > 5):
+	#	break
+	
+	link_tag = driver.find_elements_by_xpath("//a[@href]")
+	link = link_tag[5:17]
+	for i in link:
+				x = i.get_attribute('href')
+				all_links.append(x)
+	#navigate to the next page
+	next_pg = driver.find_element_by_link_text('Next')
+	next_pg.click()
+	time.sleep(random.randint(15, 30))
+
+for alpha in all_links:
+	driver.get(alpha+"/transactions")
+	time.sleep(2)
+	downloadcsv_button = driver.find_element_by_xpath('//a[@class = "download-csv"]')
+	downloadcsv_button.click()
+
+	time.sleep(2)
+
+	start_date = driver.find_element_by_xpath("//input[contains(@class, 'form-control')]")
+
+	time.sleep(5)
+	start_date.click()
+	start_date.clear()
+	start_date.send_keys("01/01/2000")
+	download_button = driver.find_element_by_xpath("//button[@class='btn btn-sm btn-primary']")
+	download_button.click()
+	time.sleep(20)
