507465fab3de42a906c972834d9b00c7a517651a
Nameetharamachandra <nameethapatel@gmail.com>
2018-12-29 16:53:52 -0500

    updated path

--- folder/path.py
+++ folder/path.py
@@ -3,4 +3,4 @@ Json_Mohit='/Users/mohitdedhe/Desktop/MohitsDocs/Certification and Projects/VPro
 Json_Vivek='/home/vivek/github-data'
 Json_Arundathi='C:/Users/arupatil/Documents/Data Analytics - Github/Data'
 Json_Malathy='D:/MS BAIS/GitHub/Data'
-Json_Nameetha=''
+Json_Nameetha='C://Users/Nameetha/Desktop/Github'
