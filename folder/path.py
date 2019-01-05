ab6dd038290385baddfe4c0ff6f78f43a8b25a59
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2019-01-05 00:18:47 -0500

    Update path.py

--- folder/path.py
+++ folder/path.py
@@ -1,6 +1,6 @@
 
 Json_Mohit='/Users/mohitdedhe/Desktop/MohitsDocs/Certification and Projects/VProject/Files'
-Json_Vivek='/home/vivek/github-data'
+Json_Vivek='/home/vivek/github-data_2017'
 Json_Arundathi='C:/Users/arupatil/Documents/Data Analytics - Github/Data'
 Json_Malathy='D:/MS BAIS/GitHub/Data'
 Json_Nameetha='C://Users/Nameetha/Desktop/Github'
