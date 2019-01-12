8796d0923d382d905bafb56c8121ffcd5cc53330
Arundhathi Patil <37865001+arundhathi-patil@users.noreply.github.com>
2019-01-11 19:06:37 -0500

    Update getGithubDataUsingSeleniumWebdriver.py

--- opencollective/getGithubDataUsingSeleniumWebdriver.py
+++ opencollective/getGithubDataUsingSeleniumWebdriver.py
@@ -28,33 +28,62 @@ for link in df["GitHub_Url"]:
     driver.get(link)
     x = driver.find_elements_by_xpath("//span[@class='num text-emphasized']")
     time.sleep(2)
-    list1 = []    
-    for i in x:
-        list1.append(i.text)
-    commits.append(list1[0])
-    branches.append(list1[1])
-    releases.append(list1[2])
-    contributors.append(list1[3])
+    list1 = []  
+    try:
+    
+        
+        for i in x:
+            list1.append(i.text)
+        commits.append(list1[0])
+        branches.append(list1[1])
+        releases.append(list1[2])
+        contributors.append(list1[3])
+        
+    except:
+       
+       commits.append(0)
+       branches.append(0)
+       releases.append(0)
+       contributors.append(0)
+       
     
     y = driver.find_elements_by_xpath("//span[@class='Counter']")
     time.sleep(2)
     list2 = []
-    for i in y:
-        list2.append(i.text)
-    issues.append(list2[0])
-    pulls.append(list2[1])
+    try:
+        for i in y:
+            list2.append(i.text)
+        issues.append(list2[0])
+        pulls.append(list2[1])
+        
+    except:
+       issues.append(0)
+       pulls.append(0) 
+    
     
     z = driver.find_elements_by_xpath("//a[@class='social-count']")
     time.sleep(2)
     list3 = []
-    for i in z:
-        list3.append(i.text)
-    watches.append(list3[0])
-    forks.append(list3[1])
-    
+    try:
+        for i in z:
+            list3.append(i.text)
+        watches.append(list3[0])
+        forks.append(list3[1])
+        
+    except:
+        watches.append(0)
+        forks.append(0)
+        
+        
+
     s = driver.find_element_by_xpath("//a[@class = 'social-count js-social-count']")
     time.sleep(2)
-    stars.append(s.text)
+    try:
+        stars.append(s.text)
+    except:
+        stars.append(0)
+        
+    
     
 #print(commits)
 
@@ -62,6 +91,7 @@ df["commits"] = commits
 df["branches"] = branches
 df["releases"] = releases
 df["contributors"] = contributors
+del issues[21]
 df["issues"] = issues
 df["pulls"] = pulls
 df["watches"] = watches
