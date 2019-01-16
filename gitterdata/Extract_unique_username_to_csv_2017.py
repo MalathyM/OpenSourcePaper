184fae4fba23a08b9426248a880b77c4cef1c0e4
MalathyM <42782854+MalathyM@users.noreply.github.com>
2019-01-16 02:08:47 -0500

    Create Extract_unique_username_to_csv_2017.py
    This file describes the steps to extract unique user name and to write to a csv file

---
+++ gitterdata/Extract_unique_username_to_csv_2017.py
@@ -0,0 +1,30 @@
+import os
+import glob
+import pandas as pd
+import numpy as np
+import datetime as dt
+import matplotlib.pyplot as plt
+import csv
+path = "D:/GitHub/GitHub/csv"
+allFiles = glob.glob(os.path.join(path,"*.csv"))
+np_array_list = []
+for file_ in allFiles:
+    df = pd.read_csv(file_,index_col=None, header=0)
+    np_array_list.append(df.as_matrix())
+comb_np_array = np.vstack(np_array_list)
+big_frame = pd.DataFrame(comb_np_array)
+big_frame.columns = ["sent_time","dname","uname","msg","read_by","issues"]
+big_frame['msg_count']=1
+big_frame['date'] = pd.to_datetime(big_frame['sent_time']).dt.date
+big_frame['year'] = pd.to_datetime(big_frame['sent_time']).dt.year
+big_frame['month'] = pd.to_datetime(big_frame['sent_time']).dt.month
+big_frame['day'] = pd.to_datetime(big_frame['sent_time']).dt.day
+big_frame['hour'] = pd.to_datetime(big_frame['sent_time']).dt.hour
+#create empty array
+uname_2017 = []
+uname_2017 = (year_2017['uname'].unique())
+#convert array to df for easy writing to csv
+df=pd.DataFrame(uname_2017)
+df.columns=['user_name']
+#write the data frame to csv
+df.to_csv('D:/GitHub/GitHub/uname/user_name_2017.csv',encoding='utf-8', index=False)
