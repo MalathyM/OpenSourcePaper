5e9d1331036fd7949036a8605381896d2eb75cc2
Mohit Dedhe <43933588+mdedhe@users.noreply.github.com>
2019-01-31 21:08:58 -0500

    Update GithubGitterConnect.py

--- folder/GithubGitterConnect.py
+++ folder/GithubGitterConnect.py
@@ -85,31 +85,33 @@ arr_evnt_count=dict()
 #i is used for the first iteration of the dictionary insert key and value pair
 i=0
 #getting the files list from the given path
-json_files=[json_file for json_file in os.listdir(json_path)if json_file.endswith('.json')]
-#iterating the files using For loop
-for every_file in json_files:
-    data=open(json_path+'/'+every_file)
-    monthValue=every_file[5:7]
-    #reading the data and store it to the jsonrecord for the signle file
-    jsonrecords=data.readlines()
-    data.close()
-    #iteration over the data line by line and loading to the json form
-    for line in jsonrecords:
-        jsonstrings=js.loads(line)
-        #use unique Gitter UserID and now comparing the Gitter with Github login.
-        for userid in tqdm(arrdataUserID):
-            if(jsonstrings['actor']['login']==userid):
-                #creating the dummy nodes for all the events and checking whether the userID is alread there before creating it
-                arr=createDummyNode(userid,arr,arr_evnt_count,eventType)
-                #first iteration of loop and adding first element directly to dictionary
-                eventName=jsonstrings['type']
-                if(i==0):
-                    arr_evnt_count.update({eventName:1})
-                    arr.update({userid+"_"+monthValue:arr_evnt_count})
-                    i=1
-                else:
-                    #from the next iteration checking whether the userid already present or not
-                    for key in list(arr):
+try:
+    
+    json_files=[json_file for json_file in os.listdir(json_path)if json_file.endswith('.json')]
+    #iterating the files using For loop
+    for every_file in json_files:
+        data=open(json_path+'/'+every_file)
+        monthValue=every_file[5:7]
+        #reading the data and store it to the jsonrecord for the signle file
+        jsonrecords=data.readlines()
+        data.close()
+        #iteration over the data line by line and loading to the json form
+        for line in jsonrecords:
+            jsonstrings=js.loads(line)
+            #use unique Gitter UserID and now comparing the Gitter with Github login.
+            for userid in arrdataUserID:
+                if(jsonstrings['actor']['login']==userid):
+                    #creating the dummy nodes for all the events and checking whether the userID is alread there before creating it
+                    arr=createDummyNode(userid,arr,arr_evnt_count,eventType)
+                    #first iteration of loop and adding first element directly to dictionary
+                    eventName=jsonstrings['type']
+                    if(i==0):
+                        arr_evnt_count.update({eventName:1})
+                        arr.update({userid+"_"+monthValue:arr_evnt_count})
+                        i=1
+                    else:
+                        #from the next iteration checking whether the userid already present or not
+                        for key in list(arr):
                             if(key==userid):
                                 k=arr.get(key)
                                 val=k.get(eventName)
@@ -120,6 +122,8 @@ for every_file in json_files:
                             else:
                                 arr_evnt_count.update({eventName:1})
                                 arr.update({userid+"_"+monthValue:arr_evnt_count})
+except:
+    print("done")
     
 header=['UserID', 'Month','CheckRunEvent',
 'CheckSuiteEvent',
